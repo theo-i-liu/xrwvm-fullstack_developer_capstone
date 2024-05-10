570b735d1fd029b22525a28cadb1819520fe2b98
theo-i-liu <liu__di@hotmail.com>
2024-05-10 13:32:38 -0400

    Adding temporary changes to Github

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,10 +29,12 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS =\
-    ['localhost',
-     'https://liudi-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+  ['localhost',
+   'https://liudi-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy' \
+   '.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS =\
-    ['https://liudi-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+  ['https://liudi-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy' \
+   '.cognitiveclass.ai']
 
 
 REST_FRAMEWORK = {
@@ -98,7 +100,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.UserAttributeSimilarity' \ 
+        'Validator',
     },
     {
         'NAME':
