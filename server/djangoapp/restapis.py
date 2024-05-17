e3829d164132b93f671d31420c568e4bc378a6e9
theo-i-liu <liu__di@hotmail.com>
2024-05-17 14:12:26 -0400

    Adding temporary changes to Github

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -58,3 +58,23 @@ def post_review(data_dict):
         return response.json()
     except Exception as e:
         print(f"Network exception occurred: {e}")
+
+
+def searchcars_request(endpoint, **kwargs):
+    params = ""
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params+key + "=" + value + "&"
+
+    request_url = searchcars_url+endpoint+"?"+params
+
+    print("GET from {} ".format(request_url))
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except:
+        # If any error occurs
+        print("Network exception occurred")
+    finally:
+        print("GET request call complete!")
